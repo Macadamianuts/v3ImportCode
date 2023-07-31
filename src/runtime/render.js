0f4f53be403906b852b2200a7a338021d55ceb34
WWater <2773147697@qq.com>
2023-07-31 17:01:37 +0800

    finish patch function

--- src/runtime/render.js
+++ src/runtime/render.js
@@ -40,10 +40,183 @@ const h = (type, props, children) => {
     };
 };
 
+// 判断是不是一个事件 ‘on’ 开头
+const eventReg = /^on[A-Z]/
+
 const render = (vnode, container) => {
-    container.innerHTML = '';
-    mount(vnode, container)
+    // 旧节点
+    const prevNode = container._vnode || null // 设置为null，防止初始渲染时没有旧节点
+
+    if (vnode) {
+        if (prevNode) {
+            patch(prevNode, vnode, container);
+        } else {
+            mount(vnode, container);
+        }
+        container._vnode = vnode;  // 更新当前 vnode
+    } else {
+        prevNode && unmount(prevNode);
+        container._vnode = null;
+    }
 }
+
+// processTextNode
+const processTextNode = (oldNode, newNode, container) => {
+     if(oldNode) {
+        patchTextNode(oldNode, newNode)
+    } else {
+        mountTextNode(newNode, container)
+    }
+}
+
+// processElement
+const processElement = (oldNode, newNode, container) => {
+    if(oldNode) {
+        patchElementNode(oldNode, newNode)
+    } else {
+        mountElementNode(newNode, container)
+    }
+}
+
+// patch 对比操作
+const patch = (oldNode, newNode, container) => {
+
+    if (oldNode && oldNode.type !== newNode.type) {
+        unmount(oldNode);
+        oldNode = null;
+    }
+
+    const { shapeFlag } = newNode
+    if(shapeFlag & SHAPEFLAG.TEXT) {
+        processTextNode(oldNode, newNode, container)
+    } else if(shapeFlag & SHAPEFLAG.ELEMENT) {
+        processElement(oldNode, newNode, container)
+    }
+}
+
+// patchTextNode
+const patchTextNode = (oldNode, newNode) => {
+    newNode.el = oldNode.el
+    oldNode.el.textContent = newNode.children
+}
+
+// patchElementNode
+const patchElementNode = (oldNode, newNode) => {
+    newNode.el = oldNode.el
+
+    //
+    patchProps(oldNode.props, newNode.props, newNode.el)
+    patchChildren(oldNode, newNode, newNode.el)
+}
+
+
+// 处理子节点
+const patchChildren = (oldNode, newNode, container) => {
+    const { shapeFlag: prevShapeFlag, children: prevChildren } = oldNode
+    const { shapeFlag: nextShapeFlag, children: nextChildren } = newNode
+
+    if(prevShapeFlag & SHAPEFLAG.TEXT_CHILDREN) {
+        if(nextShapeFlag & SHAPEFLAG.TEXT_CHILDREN) {
+            container.textContent = nextChildren
+        } else if(nextShapeFlag & SHAPEFLAG.ARRAY_CHILDREN) {
+            container.textContent = '';
+            mountChildren(nextChildren, container)
+        } else {
+           container.textContent = ''; 
+        }
+    } else if(prevShapeFlag & SHAPEFLAG.ARRAY_CHILDREN) {
+        if(nextShapeFlag & SHAPEFLAG.TEXT_CHILDREN) {
+            unmountChildren(prevChildren)
+            container.textContent = nextChildren
+        } else if(nextShapeFlag & SHAPEFLAG.ARRAY_CHILDREN) {
+            patchArrayChildren(prevChildren, nextChildren, container)
+        } else {
+            unmountChildren(nextChildren, container)
+        }
+    }
+}
+
+const patchArrayChildren = (prev, next, container) => {
+    const oldLength = prev.length
+    const newLength = next.length
+    const commonLength = Math.min(oldLength, newLength)
+
+    for(let i = 0; i < commonLength; i++) {
+        patch(prev[i], next[i], container)
+    }
+
+    if(oldLength > newLength) {
+        unmountChildren(prev.slice(commonLength))
+    } else if(oldLength < newLength) {  
+        mountChildren(next.slice(commonLength), container)
+    }
+}
+
+
+const patchProps = (oldProps, newProps, el) => {
+    if(oldProps === newProps) {
+        return
+    }
+
+    for(const key in oldProps) {
+        if(newProps[key] == null) {
+            patchDomProps(oldProps[key] , null , key , el)
+        }
+    }
+
+    for(const key in newProps) {
+        const prev = oldProps[key]
+        const next = newProps[key]
+
+        if(prev !== next) {
+            patchDomProps(prev , next , key , el)
+        }
+    }
+}
+
+// patchDomProps 判断方法
+const patchDomProps = (prev , next , key , el) => {
+    switch(key) {
+        case 'class':
+            el.className = next || '';
+            break;
+        case 'style':
+            if(next == null) {
+                el.removeAttribute('style')
+            } else {
+                if(prev) {
+                    for(const styleName in prev) {
+                        if(next[styleName] == null) {
+                            el.style[styleName] = ''
+                        }
+                    }
+                }
+                for(const styleName in next) {
+                    el.style[styleName] = next[styleName]
+                }
+            }
+            break;
+        default:
+            if(eventReg.test(key)) {
+                const eventName = key.slice(2).toLowerCase();
+                if(prev) {
+                    el.removeEventListener(eventName, prev)
+                }
+                if(next) {
+                    el.addEventListener(eventName, next)
+                }
+            } else {
+                if(next == null || next == false) {
+                    el.removeAttribute(key)
+                }
+                if(next) {
+                    el.setAttribute(key, next)
+                }
+            }
+            break;
+    }
+}
+
 
 const mount = (vnode, container) => {
     const { shapeFlag } = vnode;
@@ -91,17 +264,39 @@ const mountElementNode = (vnode, container) => {
 
 
 const mountChildren = (children, container) => {
-    children.forEach(child => {
-        mount(child, container)
-    });
-}
+  const commonLength = Math.min(children.length, container.childNodes.length);
+
+  for (let i = 0; i < commonLength; i++) {
+    const oldChild = container.childNodes[i];
+    const newChild = children[i];
+
+    if (shouldPatch(oldChild, newChild)) {
+      patch(oldChild._vnode, newChild, container);
+    } else {
+      mount(newChild, container);
+    }
+  }
+
+  // 如果旧子节点比新子节点多，需要将多余的旧子节点卸载
+  if (container.childNodes.length > children.length) {
+    for (let i = commonLength; i < container.childNodes.length; i++) {
+      const oldChild = container.childNodes[i];
+      unmount(oldChild._vnode);
+    }
+  }
+
+  // 如果新子节点比旧子节点多，需要将多余的新子节点挂载
+  if (children.length > container.childNodes.length) {
+    for (let i = commonLength; i < children.length; i++) {
+      const newChild = children[i];
+      mount(newChild, container);
+    }
+  }
+};
 
 const mountComponentNode = (vnode, container) => {
 
 }
-
-// 判断是不是一个事件 ‘on’ 开头
-const eventReg = /^on[A-Z]/
 
 const mountProps = (props, el) => {
     for(const key in props) {
@@ -130,22 +325,43 @@ const mountProps = (props, el) => {
     }
 }
 
-// render(
-//     h(
-//         'ul', {
-//             class: 'classul',
-//             style: {
-//                 border : '1px soild #000',
-//                 fontSize : '18px'
-//             },
-//             onClick : () => console.log('click'),
-//             id : 'idid'
-//         },
-//             [
-//                 h('li', null, 1),
-//                 h('li', null, [h(Text, null , '2')]),
-//                 h('li', null, '3'), 
-//             ]        
-//     ),
-//     document.body
-// )
+const unmount = vnode => {
+        const { el } = vnode
+        el.parentNode.removeChild(el)
+}
+
+const unmountChildren = children => {
+    children.forEach(child => {
+        unmount(child)
+    })
+}
+
+
+// test
+render(
+    h('ul', null, [
+        h('li',null, '我是 first'),
+        h('li',null, [
+            h('li',null, '我是 second的 1'),
+            h('li',null, '我是 second的 2'),
+            h('li',null, '我是 second的 3')
+        ]),
+        h('li',null, '我是 third')
+    ]),
+    document.body // 将目标容器改为 document.body
+);
+
+setTimeout(() => {
+    render(
+        h('ul', null, [
+            h('li',null, 'first'),
+            h('li',null, [
+                h('li',null, '我是 second的 first'),
+                h('li',null, '我是 second的 second'),
+                h('li',null, '我是 second的 three')
+            ]),
+            h('li',null, 'three')
+        ]),
+        document.body
+    );
+}, 3000); // 等待一段时间再执行第二个 render 函数
